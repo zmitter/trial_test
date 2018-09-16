26bad6a4d84524cb4b389316f0242efa3c03face
zmitter <mittermeyer@gmail.com>
2018-09-16 13:10:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,53 @@
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-app = Flask(__name__)
-
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('1VQGdLbDdmlpaEKonuPlg5lJczQzvQzoTYkreebQ/FQHiJMjNgt2P9CI3a2JrVQyWlnYRV2VOiXiBlM4k4u+OrQqRX74X6XKVVW0TnlOhHCm6EesFoSJXXnvi7RLAWQXjI6IkCCtCh7+L+Sjug1ktAdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('a227e7f5daf93c58b285bdcfcc5dc9e3')
-yourid='U801d402e8e9ced65a6a6d7d25c5f0fb3'
-line_bot_api.push_message(yourid, TextSendMessage(text='你可以開始了'))
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-#訊息傳遞區塊
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
-#主程式
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+##載入LineBot所需要的套件
+#from flask import Flask, request, abort
+#
+#from linebot import (
+#    LineBotApi, WebhookHandler
+#)
+#from linebot.exceptions import (
+#    InvalidSignatureError
+#)
+#from linebot.models import *
+#
+#app = Flask(__name__)
+#
+## 必須放上自己的Channel Access Token
+#line_bot_api = LineBotApi('1VQGdLbDdmlpaEKonuPlg5lJczQzvQzoTYkreebQ/FQHiJMjNgt2P9CI3a2JrVQyWlnYRV2VOiXiBlM4k4u+OrQqRX74X6XKVVW0TnlOhHCm6EesFoSJXXnvi7RLAWQXjI6IkCCtCh7+L+Sjug1ktAdB04t89/1O/w1cDnyilFU=')
+## 必須放上自己的Channel Secret
+#handler = WebhookHandler('a227e7f5daf93c58b285bdcfcc5dc9e3')
+#yourid='U801d402e8e9ced65a6a6d7d25c5f0fb3'
+#line_bot_api.push_message(yourid, TextSendMessage(text='你可以開始了'))
+#
+## 監聽所有來自 /callback 的 Post Request
+#@app.route("/callback", methods=['POST'])
+#def callback():
+#    # get X-Line-Signature header value
+#    signature = request.headers['X-Line-Signature']
+#
+#    # get request body as text
+#    body = request.get_data(as_text=True)
+#    app.logger.info("Request body: " + body)
+#
+#    # handle webhook body
+#    try:
+#        handler.handle(body, signature)
+#    except InvalidSignatureError:
+#        abort(400)
+#
+#    return 'OK'
+#
+##訊息傳遞區塊
+#@handler.add(MessageEvent, message=TextMessage)
+#def handle_message(event):
+#    message = TextSendMessage(text=event.message.text)
+#    line_bot_api.reply_message(event.reply_token,message)
+#
+##主程式
+#import os
+#if __name__ == "__main__":
+#    port = int(os.environ.get('PORT', 5000))
+#    app.run(host='0.0.0.0', port=port)
+#
 
 
 
@@ -160,7 +160,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('1VQGdLbDdmlpaEKonuPlg5lJczQzvQzoTYkreebQ/FQHiJMjNgt2P9CI3a2JrVQyWlnYRV2VOiXiBlM4k4u+OrQqRX74X6XKVVW0TnlOhHCm6EesFoSJXXnvi7RLAWQXjI6IkCCtCh7+L+Sjug1ktAdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('a227e7f5daf93c58b285bdcfcc5dc9e3')
-yourid='1608027694'
+yourid='U801d402e8e9ced65a6a6d7d25c5f0fb3'
 line_bot_api.push_message(yourid, TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
--- mongodb.py
+++ mongodb.py
@@ -11,7 +11,7 @@ import urllib.parse
 import datetime
 
 username = urllib.parse.quote_plus('test') 
-password = urllib.parse.quote_plus('ki61J7W1')
+password = urllib.parse.quote_plus('test5566')
 host = 'ds163870.mlab.com' #主機位置
 port = '63870' #port號碼
 dbname='trial_test'
